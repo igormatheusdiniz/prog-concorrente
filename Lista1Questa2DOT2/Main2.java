511a96be2257eaff21baf63863908283ebc612f4
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-11-26 11:27:31 -0300

    one more

--- Lista1Questa2DOT2/Main2.java
+++ Lista1Questa2DOT2/Main2.java
@@ -11,20 +11,21 @@ public class Main2 {
 		final Server server1 = new Server("mirror1.com");
 		final Server server2 = new Server("mirror2.br");
 		//final Server server3 = new Server("mirror3.edu");
-		new Thread(new Requester(server1)).start();
-		new Thread(new Requester(server2)).start(); 
+		new Thread(new Requester(server1, server2)).start();
+		//new Thread(new Requester(server2)).start(); 
 		// new Thread(new Requester(server2, server1, server3)).start();
 		// new Thread(new Requester(server3 ,server2, server1)).start();
 	}
 
 	static class Requester implements Runnable {
-		private Server server;
-		//private Server server2;
+		private Server server1;
+		private Server server2;
 		private APIRequester api = new APIRequester();
 		// private Server server3;
 
-		public Requester(Server server) {
-			this.server = server;
+		public Requester(Server server1, Server server2) {
+			this.server1 = server1;
+			this.server2 = server2; 
 			//this.server2 = server2;
 			// this.server3 = server3;
 		}
@@ -36,7 +37,7 @@ public class Main2 {
 					Thread.sleep(random.nextInt(5));
 				} catch (InterruptedException e) {
 				}
-				api.reliableRequest(server);
+				api.reliableRequest(server1, server2);
 				//api.reliableRequest(server2);
 			}
 		}
@@ -65,15 +66,15 @@ public class Main2 {
 			// TODO Auto-generated constructor stub
 		}
 		
-		public boolean request(Server serverName1) {
+		public boolean request(Server serverName1, Server serverName2) {
 			Boolean server1 = false;
-			//Boolean server2 = false;
+			Boolean server2 = false;
 			// Boolean server3 = false;
 
 			try {
 				// tryLock: Acquires the lock only if it is free at the time of invocation.
 				server1 = lock.tryLock();
-				//server2 = lock.tryLock();
+				server2 = lock.tryLock();
 				// server3 = serverName.lock.tryLock();
 			} finally {
 				if (!(server1)) {
@@ -82,22 +83,22 @@ public class Main2 {
 						// unlock: Releases the lock.
 						lock.unlock();
 					}
-					//if (server2) {
-					//	currentServer = serverName2;
-					//	lock.unlock();
-				//	}
+					if (server2) {
+						currentServer = serverName2;
+						lock.unlock();
+					}
 					// if(server3) {
 					// serverName2.lock.unlock();
 					// }
 				}
 			}
-			return server1;
-			//return server1 && server2;
+			//return server1;
+			return server1 && server2;
 		}
 
-		public void reliableRequest(Server server1) {
+		public void reliableRequest(Server server1, Server server2) {
 			// Check if Lock is already exist?
-			if (request(server1)) {
+			if (request(server1, server2)) {
 				try {
 					System.out.println(currentServer.getName());
 				} finally {
