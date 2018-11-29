ffdc29558321eedc57491dd102f37a479a916252
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-11-28 23:17:45 -0300

    estrutura DONE

--- Lista1Questao7/ConsProd.java
+++ Lista1Questao7/ConsProd.java
@@ -1,19 +1,37 @@
 package Lista1Questao7;
 
-public class ConsProd implements Runnable{
-	
+public class ConsProd implements Runnable {
+
 	FilterChannel filterChannel;
 	PrintChannel printChannel;
-	
+
 	public ConsProd(FilterChannel filterChannel, PrintChannel printChannel) {
 		this.filterChannel = filterChannel;
 		this.printChannel = printChannel;
 	}
+	
+	public boolean isAlpha(String name) {     
+		return name.matches("[a-zA-Z]+"); 
+	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (true) {
+			synchronized (this.filterChannel) {
+				while (this.filterChannel.isEmpty()) {
+					try {
+						this.filterChannel.wait();
+					} catch (InterruptedException e) {
+					}
+				}
+				String takenMessage = this.filterChannel.takeMessage();
+				if (this.isAlpha(takenMessage)) {
+					this.printChannel.putString(takenMessage);
+					//System.out.println("Filtred Message: " + takenMessage);
+				}
+				this.filterChannel.notifyAll();
+			}
+		}
 	}
 
 }
