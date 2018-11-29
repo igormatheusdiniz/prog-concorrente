adec15812860420f91ab4fd26516ef27c024cdd9
igormatheusdiniz <igor.pinheiro@ccc.ufcg.edu.br>
2018-11-28 23:02:03 -0300

    questao 7 started1

--- Lista1Questao7/ConsProd.java
+++ Lista1Questao7/ConsProd.java
@@ -1,5 +1,19 @@
 package Lista1Questao7;
 
-public class ConsProd {
+public class ConsProd implements Runnable{
+	
+	FilterChannel filterChannel;
+	PrintChannel printChannel;
+	
+	public ConsProd(FilterChannel filterChannel, PrintChannel printChannel) {
+		this.filterChannel = filterChannel;
+		this.printChannel = printChannel;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
